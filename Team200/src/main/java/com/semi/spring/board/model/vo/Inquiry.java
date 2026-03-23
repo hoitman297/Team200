cc2087e1266858a46862c67b044f7ca3937f7c55
DESKTOP-7BGHVSL\user1 <donguk08165@naver.com>
2026-03-23 12:57:35 +0900

    sql 수정 / 보드매퍼 추가(공감,문의)

---
+++ Team200/src/main/java/com/semi/spring/board/model/vo/Inquiry.java
@@ -0,0 +1,20 @@
+package com.semi.spring.board.model.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Inquiry {
+	private int boardNo;
+	private int userNo;
+	private String userName;
+	private String boardContent; 
+	private String boardTitle;
+	private Date postDate;
+	private String answerStatus; // 'W' or 'C'
+	private String reason;
+	private String answer;       
+}
